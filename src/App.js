d2b95619e54da6b8a2c897c2310e4617e78e8429
Paul Buss <>
2018-09-14 07:59:09 -0500

    update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import SavageSheet from './container/SavageSheet';
 
 class App extends Component {
   render() {
@@ -8,9 +9,7 @@ class App extends Component {
         <header className="App-header">
           <h1 className="App-title">Character Tracker</h1>
         </header>
-        <p className="App-intro">
-          Content goes here
-        </p>
+        <SavageSheet />
       </div>
     );
   }
---
+++ src/container/SavageSheet.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import SavageAbilities from '../component/SavageAbilities';
+import SavageSkills from '../component/SavageSkills';
+
+class SavageSheet extends React {
+
+}
+
+export default SavageSheet;
