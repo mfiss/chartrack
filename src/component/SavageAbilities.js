bd1d7f1429384d8f73c7ed2a846c132ef4a06c92
Paul Buss <cyberserfer@gmail.com>
2018-10-16 18:28:47 -0500

    added stat stuff

--- src/component/SavageAbilities.js
+++ src/component/SavageAbilities.js
@@ -12,19 +12,70 @@ export default class SavageAbilities extends Component {
                 vigor: 8,
             }
         }
+
+        this.updateSkill = this.updateSkill.bind(this);
     }
 
+    updateAttributes(e) {
+        console.log(e);
+        this.setState({ [e.target.name]: e.target.value });
+    }
 
     render() {
         return (
             <div>
                 <h2>Attributes</h2>
                 <div>
-                    <div>Agility: </div>
-                    <div>Smarts: </div>
-                    <div>Spirit: </div>
-                    <div>Strength: </div>
-                    <div>Vigor: </div>
+                    <div>Agility:
+                        <select name="agility" defaultValue={this.state.attributes.agility} onChange={this.updateAttributes}>
+                            <option value="0">0 </option>
+                            <option value="4">4</option>
+                            <option value="6">6</option>
+                            <option value="8">8</option>
+                            <option value="10">10</option>
+                            <option value="12">12</option>
+                        </select>
+                    </div>
+                    <div>Smarts:
+                        <select name="smarts" defaultValue={this.state.attributes.smarts} onChange={this.updateAttributes}>
+                            <option value="0">0 </option>
+                            <option value="4">4</option>
+                            <option value="6">6</option>
+                            <option value="8">8</option>
+                            <option value="10">10</option>
+                            <option value="12">12</option>
+                        </select>
+                    </div>
+                    <div>Spirit:
+                        <select name="spirit" defaultValue={this.state.attributes.spirit} onChange={this.updateAttributes}>
+                            <option value="0">0 </option>
+                            <option value="4">4</option>
+                            <option value="6">6</option>
+                            <option value="8">8</option>
+                            <option value="10">10</option>
+                            <option value="12">12</option>
+                        </select>
+                    </div>
+                    <div>Strength:
+                        <select name="strength" defaultValue={this.state.attributes.spirit} onChange={this.updateAttributes}>
+                            <option value="0">0 </option>
+                            <option value="4">4</option>
+                            <option value="6">6</option>
+                            <option value="8">8</option>
+                            <option value="10">10</option>
+                            <option value="12">12</option>
+                        </select>
+                    </div>
+                    <div>Vigor:
+                        <select name="vigor" defaultValue={this.state.attributes.vigor} onChange={this.updateAttributes}>
+                            <option value="0">0 </option>
+                            <option value="4">4</option>
+                            <option value="6">6</option>
+                            <option value="8">8</option>
+                            <option value="10">10</option>
+                            <option value="12">12</option>
+                        </select>
+                    </div>
                 </div>
             </div>
         );
