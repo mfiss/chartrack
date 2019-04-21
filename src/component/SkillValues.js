ad80b83c60c1bdf3bea45fe76abb2cee86c45abe
Paul Buss <cyberserfer@gmail.com>
2019-04-21 12:19:21 -0500

    updated values

--- src/component/SkillValues.js
+++ src/component/SkillValues.js
@@ -6,13 +6,13 @@ export default class SkillValues extends Component {
     render() {
         return (
             <div>
-                        <option value="0">0 </option>
-                        <option value="4">4</option>
-                        <option value="6">6</option>
-                        <option value="8">8</option>
-                        <option value="10">10</option>
-                        <option value="12">12</option>
-                </div>
+                <option value="--">--</option>
+                <option value="4">4</option>
+                <option value="6">6</option>
+                <option value="8">8</option>
+                <option value="10">10</option>
+                <option value="12">12</option>
+            </div>
         )
-        }
+    }
 }
