c2095f43032cd7dc83ca1c4be36b045237d0808f
Paul Buss <cyberserfer@gmail.com>
2019-10-31 13:16:55 -0500

    updated package for react and put grid into savagesheet"

--- src/container/SavageSheet.js
+++ src/container/SavageSheet.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import SavageAbilities from '../component/SavageAbilities';
 import SavageSkills from '../component/SavageSkills';
 import SavageDerivedStats from '../component/SavageDerivedStats';
+import Grid from '@material-ui/core/Grid';
 
 export default class SavageSheet extends Component {
     state = {
@@ -24,33 +25,57 @@ export default class SavageSheet extends Component {
         }
     }
 
-    updateAttributes = e => this.setState({ attributes: {
-        ...this.state.attributes,
-        [e.target.name]: e.target.value } });
+    updateAttributes = e => this.setState({
+        attributes: {
+            ...this.state.attributes,
+            [e.target.name]: e.target.value
+        }
+    });
 
-    updateSkills = e => this.setState({ skills: {
-        ...this.state.skills,
-        [e.target.name]: e.target.value } });
+    updateSkills = e => this.setState({
+        skills: {
+            ...this.state.skills,
+            [e.target.name]: e.target.value
+        }
+    });
 
     render() {
         return (
-            <ul className='sheetColumns'>
-                <li>
-                    <SavageAbilities 
-                        updateAttributes = {this.updateAttributes}
-                        attributes={this.state.attributes}
-                    />
-                    <SavageDerivedStats
-                        baseStats={this.state}
-                     />
-                </li>
-                <li>
-                    <SavageSkills 
-                        updateSkills = {this.updateSkills}
-                        skills = {this.state.skills}
-                    />
-                </li>
-            </ul>
+            <span>
+                <Grid container spacing={3}>
+                    <Grid item xs={1}>
+                        <span />
+                    </Grid>
+                    <Grid item xs={5}>
+                        <SavageAbilities
+                            updateAttributes={this.updateAttributes}
+                            attributes={this.state.attributes}
+                        />
+                    </Grid>
+                    <Grid item xs={6}>
+                        <SavageDerivedStats
+                            baseStats={this.state}
+                        />
+                    </Grid>
+                    <Grid item xs={1}>
+                        <span />
+                    </Grid><Grid item xs={5}>
+                        <SavageSkills
+                            updateSkills={this.updateSkills}
+                            skills={this.state.skills}
+                        />
+                    </Grid>
+                    <Grid item xs={6}>
+                        <span />
+                    </Grid>
+                </Grid>
+                <ul className='sheetColumns'>
+
+                    <li>
+
+                    </li>
+                </ul>
+            </span>
         );
     }
 }
